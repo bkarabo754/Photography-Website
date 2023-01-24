1a616faaa1ef846af38c84cedf4574a801c92df0
bkarabo754 <bkmasango26@gmail.com>
2023-01-24 19:11:13 +0200

    About & Home Section

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import { ImFacebook, ImTwitter, ImPinterest, ImInstagram, ImYoutube} from 'react-icons/im'
 
 const Socials = () => {
   return (
     <div className='hidden xl:flex ml-24'>
-
+       <ul className='flex gap-x-4'>
+        <li>
+          <a href='http://wwww.facebook.com' target='_blank'>
+            <ImFacebook />
+          </a>
+        </li>
+        <li>
+          <a href='http://www.twitter.com' target='_blank'>
+            <ImTwitter />
+          </a>
+        </li>
+        <li>
+          <a href='http://www.pinterest.com' target='_blank'>
+            <ImPinterest />
+          </a>
+        </li>
+        <li>
+          <a href='http://www.instagram.com' target='_blank'>
+            <ImInstagram />
+          </a>
+        </li>
+        <li>
+          <a href='http://www.youtube.com' target='_blank'>
+            <ImYoutube />
+          </a>
+        </li>
+      </ul>
     </div>
-
-  )
-  
+  );
 };
 
 export default Socials;
